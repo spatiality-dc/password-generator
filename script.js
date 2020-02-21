84c7f4a383f2a34079550666e500fea6e4acbfd1
spatiality-dc <dan@dinosaurmedia.org>
2020-02-21 20:40:30 +1100

    Random number generator fixed, but with a set letterBag variable

--- script.js
+++ script.js
@@ -1,64 +1,37 @@
 //Valid password symbols
-const lower = "abcdefghijklmnopqrstuvwxyz";
-const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const numbers = "0123456789";
-const symbols = "!@#%^&*=-_";
+//const lower = "abcdefghijklmnopqrstuvwxyz";
+//const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//const numbers = "0123456789";
+//const symbols = "!@#%^&*=-_";
 
 // Assignment Code
 const generateBtn = document.querySelector("#generate");
 const numEL = document.querySelector("#num");
 const lowerEL = document.querySelector("#lower");
-//const lengthEl
-
-console.log(generatePassword(12, lower));
+const upperEL = document.querySelector("#upper");
+const symEL = document.querySelector("#sym");
+var lengthEl = document.querySelector("#length");
+var newPassword = document.getElementById("#password");
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", function() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
+  var letterBag = lower;
+  numEL.checked ? (letterBag += numbers) : "";
+  upperEL.checked ? (letterBag += upper) : "";
+  symEL.checked ? (letterBag += symbols) : "";
+  newPassword.value = password(lengthEl.value, letterbag);
 });
 
-function generatePassword(l, characters) {
-  var letterBag = "";
-  for (var i = 0; i < l; i++) {
-    letterBag += characters.charAt(
-      Math.floor(Math.random() * characters.length)
-    );
-    return letterBag;
+function generatePassword() {
+  var length = 8,
+    letterBag =
+      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789~!@#$%^&*",
+    retVal = "";
+  for (var i = 0, n = letterBag.length; i < length; ++i) {
+    retVal += letterBag.charAt(Math.floor(Math.random() * n));
   }
-
-  debugger;
-
-  // if (lowerEL.checked) {
-  //   letterBag += lower;
-  // }
-
-  if (numEL.checked) {
-    letterBag += numbers;
-  }
-
-  var result = "";
-
-  for (let letter = 0; letter < charNumEl.valueAsNumber; letter++) {
-    //result +=
-  }
-
-  return result;
+  return retVal;
 }
-
-//Check charNum box for value
-
-//Use value of charNum to determine password length
-
-//Is num box ticked?
-
-//If yes, add numbers to random generator array
-
-//Is sym box ticked?
-
-//If yes, add numbers to random generator array
-
-//Once all elements checked, eventListener
-
-//writePassword
